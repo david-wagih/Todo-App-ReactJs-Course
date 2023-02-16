259459a3de6508917a58e34cc29144737d1fe0a2
David Wagih <davidwagih62@gmail.com>
2023-02-16 17:47:08 +0200

    adding more component as Modal and backdrop

---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Modal from "../components/Modal";
+import Backdrop from "../components/Backdrop";
+
+function Todo(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  function deleteHandler() {
+    setShowModal(true);
+  }
+
+  function closeModalHandler() {
+    setShowModal(false);
+  }
+
+  return (
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
+      </div>
+      {showModal && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {showModal && <Backdrop onClick={closeModalHandler} />}
+    </div>
+  );
+}
+
+export default Todo;
